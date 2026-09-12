ec6a6f6e184eecbedbfdc1125afb7ab0c8e4ed98
Michael Varyuschenko <varyuschenko.mk@phystech.edu>
2026-09-12 16:16:18 +0300

    Task 1.3.1  done

---
+++ 131-hello-usb/main.c
@@ -0,0 +1,20 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include <stdio.h>
+
+const uint LED_PIN = 25;
+
+int main() {
+    //gpio_init(LED_PIN);
+    //gpio_set_dir(LED_PIN, GPIO_OUT);
+    stdio_init_all();
+
+    while (1) {
+        /*gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);*/
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
