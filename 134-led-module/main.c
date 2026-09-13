b782103863776b2d860311610cc6082b5073379b
Michael Varyuschenko <varyuschenko.mk@phystech.edu>
2026-09-13 13:49:33 +0300

    Task 1.3.7 done

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -4,6 +4,7 @@
 
 #include "led.h"
 #include "log.h"
+#include "device.h"
 
 const uint BUTTON_PIN = 15;
 const uint DEBOUNCE_MS = 200;
@@ -27,6 +28,9 @@ void handle_command(int command) {
         }
         else if (command == 'v') {
             log_version();
+        }
+        else if (command == 'i') {
+            device_info();
         }
         else {
             LOG_ERR("unknown command: %c\n", command);
